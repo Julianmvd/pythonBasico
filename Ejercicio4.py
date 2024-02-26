b44046e7af20dbb19a2f66ab7671ceaf6906ebff
Julianmvd <160616208+Julianmvd@users.noreply.github.com>
2024-02-25 21:07:14 -0500

    Ejercicio4.py

---
+++ Ejercicio4.py
@@ -0,0 +1,7 @@
+n1 = float(input("Ingrese el número: "))
+if n1==2:
+    print("El número es primo ")
+elif (n1%2) == 0:
+    print("El número no es primo ")
+elif (n1%2) == 1:
+    print("El número es primo ")
